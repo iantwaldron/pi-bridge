bac3f7122b082d520b9b8761b0970adf757503da
Ian Waldron <ian@waldrontech.com>
2026-02-08 13:22:00 -0800

    Centralize common defaults across scripts

---
+++ setup/defaults.sh
@@ -0,0 +1,8 @@
+# Shared defaults for setup scripts
+# Source this file at the top of each script
+
+DEFAULT_WIFI_CHIPSET="intel"
+DEFAULT_AP_INTERFACE="wlan1"
+DEFAULT_AP_SSID="PiNet"
+DEFAULT_AP_COUNTRY="US"
+DEFAULT_AP_GATEWAY="192.168.31.4"
